85f1ae4c8a8e4ce5fff3ea9860a93cb0c8b1b637
simonaditia <smnsitumeang@gmail.com>
2023-09-19 11:36:44 +0700

    First commit, install package & create simple server

---
+++ app.js
@@ -0,0 +1,12 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const port = 3000 || process.env.PORT
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})

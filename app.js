57db9078695f8a1e88b4bc2299828b2df4135d96
simonaditia <smnsitumeang@gmail.com>
2023-09-19 11:48:25 +0700

    Use ejs for templating engine

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 require("dotenv").config()
 const express = require("express")
+const expressLayout = require("express-ejs-layouts")
 const app = express()
 const port = 3000 || process.env.PORT
 
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
+
+app.use(express.static("public"))
+app.use(expressLayout)
+app.set("layout", "./layouts/main")
+app.set("view engine", "ejs")
+
 app.get("/", (req, res) => {
-    res.send("Hello World!")
+    res.render("index")
 })
 
 app.listen(port, () => {

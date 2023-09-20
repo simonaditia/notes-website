545459372c385d3dfaf80aae7cd649c27cb6c4d4
simonaditia <smnsitumeang@gmail.com>
2023-09-20 13:07:16 +0700

    Create user model, login using google auth (passport)

--- app.js
+++ app.js
@@ -2,10 +2,26 @@ require("dotenv").config()
 const express = require("express")
 const expressLayout = require("express-ejs-layouts")
 const connectDB = require("./servers/config/db")
+const session = require('express-session')
+const passport = require('passport')
+const MongoStore = require('connect-mongo')
+const authRoute = require('./servers/routes/auth')
 const mainRoute = require("./servers/routes/index")
 const dashboardRoute = require("./servers/routes/dashboard")
 const app = express()
 const port = 3000 || process.env.PORT
+
+app.use(session({
+    secret: "Keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    })
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use(express.urlencoded({
     extended: true
@@ -21,6 +37,7 @@ app.set("layout", "./layouts/main")
 app.set("view engine", "ejs")
 
 app.use("/", mainRoute)
+app.use("/", authRoute)
 app.use("/dashboard", dashboardRoute)
 
 // Handle 404
